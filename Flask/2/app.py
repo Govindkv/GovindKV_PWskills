9ff89b31f69566d36b626e53805a7ac12cfce12a
Govindkv <150502731+Govindkv@users.noreply.github.com>
2024-01-11 11:39:31 +0530

    2 app.py

--- Flask/2/app.py
+++ Flask/2/app.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/govind")
+def home_page():
+    return "This is my homepage"
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 8000)
